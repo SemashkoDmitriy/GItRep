cdb0da59e4815fb6aa57e62e42e2085e6ac7632a
Dmitriy Semashko <semashko@fgoupsk.ru>
2023-02-10 11:07:09 +0300

    Добавил решение задания 2.1.5 курса

---
+++ lesson 2.1.5.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+link = "https://suninjuly.github.io/math.html"
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    c1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    c1.click()
+    c2 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    c2.click()
+    answer = browser.find_element(By.CSS_SELECTOR, "#answer")
+    print(y)
+    answer.send_keys(y)
+    button = browser.find_element(By.XPATH, "//button[contains(text(), 'Su')]")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
