d2cee0b98a2984333373e385e2607fd1f7ed8096
Dmitriy Semashko <semashko@fgoupsk.ru>
2023-02-13 16:08:59 +0300

    Изменил решение задания 2.1.5 курса

--- lesson 2.1.5.py
+++ lesson 2.1.5.py
@@ -3,15 +3,15 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
-link = "https://suninjuly.github.io/math.html"
+link = "http://suninjuly.github.io/get_attribute.html"
 def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
-    x = x_element.text
+    x_element = browser.find_element(By.ID, "treasure")
+    x = x_element.get_attribute("valuex")
     y = calc(x)
     c1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
     c1.click()
@@ -22,6 +22,14 @@ try:
     answer.send_keys(y)
     button = browser.find_element(By.XPATH, "//button[contains(text(), 'Su')]")
     button.click()
+    time.sleep(5)
+
+    people_radio = browser.find_element(By.ID, "peopleRule")
+    people_checked = people_radio.get_attribute("checked")
+    print("value of people radio: ", people_checked)
+    assert people_checked is not None, "People radio не выбран по умолчанию"
+
+
 
 finally:
     # успеваем скопировать код за 30 секунд
