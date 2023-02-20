ab613d7a7dff1257dafd709e9e746cfe9b474127
Dmitriy Semashko <semashko@fgoupsk.ru>
2023-02-20 14:02:02 +0300

    Добавил решения заданий 2.2.6-2.2.7 курса

--- lesson 2.2.5.py
+++ lesson 2.2.5.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -5,4 +7,6 @@ browser = webdriver.Chrome()
 link = "https://SunInJuly.github.io/execute_script.html"
 browser.get(link)
 button = browser.find_element(By.TAG_NAME, "button")
+_ = button.location_once_scrolled_into_view
+time.sleep(5)
 button.click()
