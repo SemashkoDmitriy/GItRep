10d87f58279581a4bd8155140b922c2c73c73876
Dmitriy Semashko <semashko@fgoupsk.ru>
2023-03-13 16:33:59 +0300

    Добавил решения задания 3.2.7 курса

---
+++ lesson 3.2.7.py
@@ -0,0 +1,8 @@
+actual_result = -42
+assert abs(-42) == actual_result , 'Wrong result, got ' + str(actual_result) + ', smth wrong'
+one = 123
+str1 = "one"
+str2 = "two"
+str3 = "three"
+print("Let's count together: {}, then goes {}, and then {}".format(one, str2, "three"))
+print(f"Let's count together: {one}, then goes {str2}, and then {str3}")
